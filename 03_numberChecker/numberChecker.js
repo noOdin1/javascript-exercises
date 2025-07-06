acc476796f89d1eae1391defdf140dc501b37319
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-07 02:52:54 +0800

    All conditions passed, edited js & editing spec

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,5 +1,10 @@
 function numberChecker(number) {
-  if (number === 6) {
+  // if (number === 6) {  // original
+  // if (number === 1000) {  // 1 exercise
+  // if (number === 10) { // 2 exercise
+  // if (number === 7) { // 3 exercise
+  // if (number === 7) { // 4 exercise
+  if (number === 7) {
     return true;
   } else {
     return false;
