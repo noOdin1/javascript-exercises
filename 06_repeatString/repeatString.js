c73c56a00478b73b4a527fcce9f839ede04d1cce
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-15 01:57:25 +0800

    Completed all the exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  // var inc = "";
+  var inc = num > 0 ? "" : "ERROR";
+  if (num == 0) return "";
+  for (var i = 0; i < num; i++) {
+    inc += word;
+  }
+  return inc;
 };
 
 // Do not edit below this line
