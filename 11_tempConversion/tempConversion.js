ad67415b38a78ed102041dc00e46c8260394081b
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-16 03:57:17 +0800

    Coded the solution and passed all the test

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (value) {
+  return parseFloat((Math.round((value - 32) * (5 / 9) * 10) / 10).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (value) {
+  return parseFloat((Math.round((value * (9 / 5) + 32) * 10) / 10).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
