3dc0a7f3917ec9786db80963c5336bc5c04cd937
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-16 02:17:37 +0800

    Coded the solution and passed all the test

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a != "number" || typeof b != "number") return "ERROR";
+  if (a < 0 || b < 0 || a % 1 != 0 || b % 1 != 0) return "ERROR";
+  if (b < a) {
+    [a, b] = [b, a]; // alt: b = [a, a = b][0];
+  }
+  console.log(`a = ${a}, b = ${b}`);
 
+  const arrayRange = (start, stop, step) =>
+    Array.from(
+      { length: (stop - start) / step + 1 },
+      (value, index) => start + index * step,
+    );
+
+  return arrayRange(a, b, 1).reduce((a, b) => a + b, 0);
 };
 
 // Do not edit below this line
