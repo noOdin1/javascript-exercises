a47b4bb9b93120d15adaad8e94fcb0ce1ba38854
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-15 22:40:39 +0800

    Finished the function and passed all test

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (list, ...toRemove) {
+  let toRemoveLength = toRemove.length;
+  let joinedList = toRemove.concat(list);
+
+  retList = joinedList.filter(
+    (elem, index) => joinedList.indexOf(elem) === index,
+  );
+
+  retList.splice(0, toRemoveLength);
+  return retList;
 };
 
 // Do not edit below this line
