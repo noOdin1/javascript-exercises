6898d26b3c4fd6a078ca8c57ea6c40d1589bbd33
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-16 17:26:05 +0800

    Added comments on the solution I provided, drawbacks

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -2,11 +2,18 @@ const removeFromArray = function (list, ...toRemove) {
   let toRemoveLength = toRemove.length;
   let joinedList = toRemove.concat(list);
 
+  // This method has a drawback. If the original array have duplicates but
+  // the items removed are not the part of the arrays' duplicate, then
+  // the duplicates in the array will be removed except for one.
   retList = joinedList.filter(
     (elem, index) => joinedList.indexOf(elem) === index,
   );
 
-  retList.splice(0, toRemoveLength);
+  retList.splice(0, toRemoveLength); // This cannot be:
+  //   return retList.splice(0, toRemoveLength);
+  // Because this would 'return' would give us the 'spliced' items
+  // retList has been modified through the 'spliced' so,
+  // we return retList
   return retList;
 };
 
