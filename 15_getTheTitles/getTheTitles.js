adbb07d012184286e585c57ca0c07924f09141af
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-22 05:38:10 +0800

    Coded the solution and passed the test. Longer than the solution.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+  let retArray = [];
+  obj.forEach((item) => {
+    for (let key in item) {
+      console.log(`[objectLoop] key: ` + key + `, item[${key}]: ` + item[key]);
+      if (key == "title") {
+        retArray.push(item[key]);
+      }
+    }
+  });
+  return retArray;
 };
 
 // Do not edit below this line
