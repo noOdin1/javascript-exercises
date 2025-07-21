56f82069306dc026c62cee0e631adaf2d8ccd36d
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-22 03:28:47 +0800

    Coded the solution and passed all the test

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  // str = str.replace(/\s/g, "");
+  str = str.toLowerCase();
+  let tmpStr = str.split("").reverse().join("");
+  return tmpStr == str;
 };
 
 // Do not edit below this line
