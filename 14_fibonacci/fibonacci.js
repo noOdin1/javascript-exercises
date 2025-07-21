1801fc50854562a8566f762fb4b1eb8aa5539c0e
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-22 04:40:38 +0800

    Coded the solution and passed all the test

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (num == 0 || num == "0") return 0;
+  if (num == 1 || num == "1") return 1;
+  if (num == 2 || num == "2") return 1;
+  fibArray = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    let sum = fibArray[i - 1] + fibArray[i - 2];
+    fibArray.push(sum);
+  }
+  return fibArray.pop();
 };
 
 // Do not edit below this line
