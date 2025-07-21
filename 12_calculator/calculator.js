48245a98f5dab5fd39885ed2e1ed32f4e0185c5e
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-22 02:45:10 +0800

    Completed all the problems and passed the tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (sumArray) {
+  if (sumArray.length == 0) {
+    return 0;
+  }
+  if (sumArray.length == 1) return sumArray[0];
+  return sumArray.reduce((acc, currVal) => (acc += currVal), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (mulArray) {
+  return mulArray.reduce((acc, currVal) => (acc *= currVal), 1);
 };
 
-const power = function() {
-	
+const power = function (num, exp) {
+  let res = 1;
+  for (let i = 0; i < exp; i++) {
+    res *= num;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact = 1;
+  if (num != 1 || num != 0) {
+    for (let i = 1; i <= num; i++) {
+      fact *= i;
+    }
+  }
+  return fact;
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
