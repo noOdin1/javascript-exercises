11ac36087d5482facbcf9c01a1b32529575e8161
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-22 18:32:44 +0800

    Coded the solution and passed all the test

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (persons) {
+  let oldestPerson = "";
+  let oldestAge = 0;
+  persons.forEach((person) => {
+    let age =
+      (person.yearOfDeath === undefined
+        ? new Date().getFullYear()
+        : person.yearOfDeath) - person.yearOfBirth;
+    if (oldestAge < age) {
+      oldestAge = age;
+      oldestPerson = person;
+    }
+  });
+  // let rightPerson = persons.filter((person) =>
+  //     person.name == oldestPerson,
+  //   );
+  // };
+  // console.log("rightPerson name: " + rightPerson.name);
+  // return rightPerson;
+  return oldestPerson;
 };
 
 // Do not edit below this line
