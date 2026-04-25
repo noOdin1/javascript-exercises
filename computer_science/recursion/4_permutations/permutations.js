af15c967ed49791fe385d2cb69744ff8b24b9ccf
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-25 23:07:47 +0800

    Further refactoring, removing unused codes

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,17 +1,3 @@
-// const isStringPrimitive = (str) => typeof str === "string";
-//
-// // This will test if 'num' is strictly a number
-// const isNumberPrimitive = (num) => Number.isFinite(num);
-//
-// // Check if 'num' is an integer
-// const isInteger = (num) => Number.isInteger(num);
-//
-// // Test to is if 'val' is an array
-// const isAnArray = (val) => Array.isArray(val);
-//
-// // test to see if 'obj' is an object
-// const isStrictObject = (obj) => obj?.constructor === Object;
-
 const permutations = (arr) => {
   if (arr.length <= 1) {
     return [arr];
