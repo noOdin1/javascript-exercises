3a36e76e68261100a46d4663fa836db7d6e4bce1
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-25 23:02:44 +0800

    Refactored the code

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,52 +1,34 @@
-const isStringPrimitive = (str) => typeof str === "string";
-
-// This will test if 'num' is strictly a number
-const isNumberPrimitive = (num) => Number.isFinite(num);
-
-// Check if 'num' is an integer
-const isInteger = (num) => Number.isInteger(num);
-
-// Test to is if 'val' is an array
-const isAnArray = (val) => Array.isArray(val);
-
-// test to see if 'obj' is an object
-const isStrictObject = (obj) => obj?.constructor === Object;
-
-const swapPos = (arr) => {
-  return [arr[1], arr[0]];
-};
-
-const addItemToArray = (item, arr) => {
-  let tmpItem = [item];
-  arr.forEach((val) => {
-    tmpItem.push(val);
-  });
-  return tmpItem;
-};
+// const isStringPrimitive = (str) => typeof str === "string";
+//
+// // This will test if 'num' is strictly a number
+// const isNumberPrimitive = (num) => Number.isFinite(num);
+//
+// // Check if 'num' is an integer
+// const isInteger = (num) => Number.isInteger(num);
+//
+// // Test to is if 'val' is an array
+// const isAnArray = (val) => Array.isArray(val);
+//
+// // test to see if 'obj' is an object
+// const isStrictObject = (obj) => obj?.constructor === Object;
 
 const permutations = (arr) => {
-  // if (!isAnArray(arr)) {
-  //   return undefined;
-  // }
   if (arr.length <= 1) {
     return [arr];
   }
   let possiblePos = [];
   if (arr.length == 2) {
     // base case the case to break out of the recursion
-    let [a, b] = [swapPos(arr)];
-    return [arr, swapPos(arr)];
+    return [arr, arr.toReversed()];
   }
 
   arr.forEach((val, index) => {
-    let tmpArr = permutations(arr.toSpliced(index, 1));
-
-    tmpArr.forEach((arrStack) => {
-      possiblePos.push(addItemToArray(val, arrStack));
+    permutations(arr.toSpliced(index, 1)).forEach((arrStack) => {
+      arrStack.unshift(val);
+      possiblePos.push(arrStack);
     });
   });
   return possiblePos;
 };
-
 // Do not edit below this line
 module.exports = permutations;
